9c5384e6b2a85a14534178858327d07f518c90c8
Vsevolod Burtyk <newemailaddress999999@gmail.com>
2025-05-21 13:07:03 +0300

    add tests for Category and Recipe models

--- recipe/tests.py
+++ recipe/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .models import Category, Recipe
 
-# Create your tests here.
+class CategoryModelTest(TestCase):
+    def test_create_category(self):
+        category = Category.objects.create(name="Dessert")
+        self.assertEqual(str(category), "Dessert")
+
+    def test_iter_method(self):
+        category = Category.objects.create(name="Soups")
+        self.assertEqual(list(iter(category)), ["Soups"])
+
+
+class RecipeModelTest(TestCase):
+    def test_create_recipe(self):
+        category = Category.objects.create(name="Drinks")
+        recipe = Recipe.objects.create(
+            title="Lemonade",
+            description="Refreshing drink",
+            instructions="Mix water, lemon and sugar",
+            ingredients="Water, Lemon, Sugar",
+            category=category
+        )
+        self.assertEqual(str(recipe), "Lemonade")
+        self.assertEqual(recipe.category.name, "Drinks")
